a126f755647ab1434a06d3fbe336d56d84f8f34d
Emir Bugra Koksalan <kodmanyagha@gmail.com>
2026-09-22 21:48:48 +0300

    deploy script fix

--- deploy.sh
+++ deploy.sh
@@ -2,14 +2,14 @@
 
 git pull origin main
 
-kill $(pgrep -f tavla_api)
-
 sea-orm-cli migrate
 
 cargo build --release
 
 ./target/release/seed
 
+mkdir -p logs
+kill $(pgrep -f tavla_api)
 (nohup ./target/release/tavla_api 2>&1) >> logs/tavla_api.log &
 
 sleep 3
